9a854b852292867bfd6cebc9a61137d3b2ad1f8c
Forrest Jones <forrestjones2010@gmail.com>
2023-02-22 16:07:46 -0600

    GDC6

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length < 1:
+        return []
+    elif length == 1:
+        return [0]
+    elif length == 2:
+        return [0, 1]
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < length:
+            next_num = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_num)
+        return fib_sequence
+
+# example usage
+fib_sequence = print_fibonacci(9)
+print(fib_sequence)
+
+#GDC
